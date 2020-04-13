cb95ecdf91cc2d42127632ba17542ec853201040
Edson Rodrigues <edinho_rodrigues@hotmail.com>
2020-04-13 09:38:28 -0300

    varias

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-/* eslint-disable import/no-unresolved */
 import React from 'react';
+import { Router } from 'react-router-dom';
+import './config/ReactotronConfig';
+
+import Routes from './routes';
+import history from './services/history';
 
 function App() {
-    return <div className="App" />;
+    return (
+        <Router history={history}>
+            <Routes />
+        </Router>
+    );
 }
 
 export default App;
