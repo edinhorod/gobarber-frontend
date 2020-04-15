ba8d7c0d44a4caba5e359987bd6ae4122d033d18
Edson Rodrigues <edinho_rodrigues@hotmail.com>
2020-04-15 09:25:51 -0300

    varios

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,8 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
+import * as Yup from 'yup';
 
 import logo from '../../assets/logo.svg';
+
+const schema = Yup.object().shape({
+    name: Yup.string().required('Nome é obrigatório'),
+    email: Yup.string()
+        .email('E-mail inválido')
+        .required('E-mail é Obrigatório'),
+    password: Yup.string()
+        .min(6, 'A senha deve ter pelo mneos 6 caracteres')
+        .required('Senha é obrigatória'),
+});
 
 export default function SignUp() {
     function handleSubmit(data) {
@@ -11,7 +22,7 @@ export default function SignUp() {
     return (
         <>
             <img src={logo} alt="BoBarber" />
-            <Form onSubmit={handleSubmit}>
+            <Form schema={schema} onSubmit={handleSubmit}>
                 <Input name="name" placeholder="Nome completo" />
                 <Input
                     name="email"
