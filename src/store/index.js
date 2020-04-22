8d5c94f59f3db02fff04b2b9c7840813874d8ede
Edson Rodrigues <edinho_rodrigues@hotmail.com>
2020-04-22 07:51:14 -0300

    store

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,25 @@
+// import createSagaMiddleware from 'redux-saga';
+// import createStore from './createStore';
+
+// import rootReducer from './modules/rootReducer';
+// import rootSaga from './modules/rootSaga';
+
+// const sagaMonitor =
+//     process.env.NODE_ENV === 'development'
+//         ? console.tron.createSagaMonitor()
+//         : null;
+// const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
+
+// const middlewares = [sagaMiddleware];
+
+// const store = createStore(rootReducer, middlewares);
+
+// createSagaMiddleware.run(rootSaga);
+
+// export default store;
+
+import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
-import createStore from './createStore';
 
 import rootReducer from './modules/rootReducer';
 import rootSaga from './modules/rootSaga';
@@ -8,12 +28,21 @@ const sagaMonitor =
     process.env.NODE_ENV === 'development'
         ? console.tron.createSagaMonitor()
         : null;
-const sagaMiddleware = createSagaMiddleware({ sagaMonitor });
 
-const middlewares = [sagaMiddleware];
+const sagaMiddleware = createSagaMiddleware({
+    sagaMonitor,
+});
 
-const store = createStore(rootReducer, middlewares);
+const enhancer =
+    process.env.NODE_ENV === 'development'
+        ? compose(
+              console.tron.createEnhancer(),
+              applyMiddleware(sagaMiddleware)
+          )
+        : applyMiddleware(sagaMiddleware);
 
-createSagaMiddleware.run(rootSaga);
+const store = createStore(rootReducer, enhancer);
+
+sagaMiddleware.run(rootSaga);
 
 export default store;
