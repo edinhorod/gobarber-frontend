cc1648a3380baf9abb365a07497208b0781d10d9
Edson Rodrigues <edinho_rodrigues@hotmail.com>
2020-04-27 09:15:18 -0300

    login da aplicação

--- src/routes/Route.js
+++ src/routes/Route.js
@@ -5,12 +5,16 @@ import { Route, Redirect } from 'react-router-dom';
 import AuthLayout from '../pages/_layouts/auth';
 import DefaultLayout from '../pages/_layouts/default';
 
+import store from '../store';
+
 export default function RouteWrapper({
     component: Component,
     isPrivate,
     ...rest
 }) {
-    const signed = false;
+    const { signed } = store.getState().auth;
+    // const signed = true;
+    console.log(signed);
 
     if (!signed && isPrivate) {
         return <Redirect to="/" />;
--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,3 +1,28 @@
-import { all } from 'redux-saga/effects';
+import { takeLatest, call, put, all } from 'redux-saga/effects';
+// import history from '~/services/history';
+import history from '../../../services/history';
+import api from '../../../services/api';
+// import api from '~/services/api';
 
-export default all({});
+import { signInSuccess } from './actions';
+
+export function* signIn({ payload }) {
+    const { email, password } = payload;
+
+    const response = yield call(api.post, 'sessions', {
+        email,
+        password,
+    });
+    console.log(response);
+
+    const { token, user } = response.data;
+    if (!user.provider) {
+        console.tron.error('Usuário não é prestador');
+        return;
+    }
+
+    yield put(signInSuccess(token, user));
+    history.push('/dashboard');
+}
+
+export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
